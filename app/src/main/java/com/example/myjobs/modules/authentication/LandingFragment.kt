38d1e418936a6484664d27e18b88466dedd2d6c7
sagar <sagar.arora@squareboat.com>
2023-01-22 19:35:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/myjobs/modules/authentication/LandingFragment.kt
@@ -0,0 +1,30 @@
+package com.example.myjobs.modules.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.myjobs.R
+import com.example.myjobs.databinding.FragmentLandingBinding
+
+
+class LandingFragment : Fragment() {
+
+lateinit var binding:FragmentLandingBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLandingBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.include.btnLoginSignup.setOnClickListener {
+            findNavController().navigate(R.id.action_landingFragment_to_loginFragment)
+        }
+    }
+}
